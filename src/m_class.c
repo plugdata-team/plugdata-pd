1e2e4be7a4c8d67af73d0601173365eddf58fc3f
Timothy Schoen <timschoen123@gmail.com>
2023-07-14 01:02:31 +0200

    Simplified mutexes

--- src/m_class.c
+++ src/m_class.c
@@ -939,6 +939,7 @@ void new_anything(void *dummy, t_symbol *s, int argc, t_atom *argv)
         tryingalready++;
         typedmess(dummy, s, argc, argv);
         tryingalready--;
+        pd_globalunlock();
         return;
     }
     class_loadsym = 0;
