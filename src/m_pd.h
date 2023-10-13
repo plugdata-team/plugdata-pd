5eeb308003db5f7480b08cc1ea8376da5cd3f318
Timothy Schoen <timschoen123@gmail.com>
2023-10-13 03:57:50 +0200

    Bump plugdata version number

--- src/m_pd.h
+++ src/m_pd.h
@@ -14,7 +14,7 @@ extern "C" {
 #define PD_BUGFIX_VERSION 0
 #define PD_TEST_VERSION ""
 
-#define PD_PLUGDATA_VERSION "0.8.0"
+#define PD_PLUGDATA_VERSION "0.8.1"
 #define PD_FLAVOR "plugdata"
 
 extern int pd_compatibilitylevel;   /* e.g., 43 for pd 0.43 compatibility */
