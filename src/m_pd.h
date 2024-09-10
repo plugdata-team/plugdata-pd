91641bd2bf598b01745244cbc94b0ddb801820a4
Timothy Schoen <timschoen123@gmail.com>
2024-09-11 01:27:58 +0200

    plugdata version bump

--- src/m_pd.h
+++ src/m_pd.h
@@ -14,7 +14,7 @@ extern "C" {
 #define PD_BUGFIX_VERSION 1
 #define PD_TEST_VERSION "test1"
 
-#define PD_PLUGDATA_VERSION "0.9.1"
+#define PD_PLUGDATA_VERSION "0.9.2"
 #define PD_FLAVOR "plugdata"
 /* compile-time version check:
    #if PD_VERSION_CODE < PD_VERSION(0, 56, 0)
